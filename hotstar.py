b5b5668da2fb7e2c67a76547e66133f3af4b2e9d
talent404 <vemulapatiharsha@gmail.com>
2017-06-24 14:27:08 +0530

    Add files via upload

---
+++ hotstar.py
@@ -0,0 +1,14 @@
+import os
+import json
+import requests
+
+hotStarUrl = raw_input('Video url:')
+tempList = hotStarUrl.split('/')
+videoId = tempList[len(tempList)-1]
+getSrc = "http://getcdn.hotstar.com/AVS/besc?action=GetCDN&asJson=Y&channel=TABLET&id="+videoId+"&type=VOD"
+resp = requests.get(getSrc)
+jsonData = json.loads(resp.text)
+videoUrl = jsonData['resultObj']['src']
+print "Paste URL and click play"
+print videoUrl
+os.system('firefox https://www.hlsplayer.net/')
